4e920ae51ff97aeb369e63580116633f091f4dd3
Bikram-sharma <65036188+Bikram-sharma@users.noreply.github.com>
2021-08-02 22:56:13 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,23 +1,26 @@
- import { useParams } from "react-router-dom";
- import usefetch from "./Usefetch";
+import { useParams } from "react-router-dom";
+import  usefetch  from './Usefetch';
 
 const BlogDetails = () => {
-  const { id } = useParams();
-  const { data: blog, error, isPending } = usefetch('http://localhost:8000/blogs/' + id);
 
-  return (
-    <div className="blog-details">
-      { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { blog && (
-        <article>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-          <div>{ blog.body }</div>
-        </article>
-      )}
-    </div>
-  );
+    const {id} = useParams();
+    
+    const { data: blogs, isPending, error} = usefetch('http://localhost:8000/blogs' + id)
+    return ( 
+        <div className = "blog-details">
+         
+        
+          {isPending && <div>Loading...</div>}
+          {error && <div>{error}</div>}
+          {blogs && (
+          <article>
+                  <h2>{blogs.title}</h2>
+                  <p>written by{blogs.author}</p>
+                  <div>{blogs.body}</div>
+              </article>
+              ) }
+           </div>
+     );
 }
  
 export default BlogDetails;
